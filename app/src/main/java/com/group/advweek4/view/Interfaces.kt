dc5df677c290654963f75a220e800953926f950a
dastyns <s160420005@student.ubaya.ac.id>
2023-06-20 17:45:54 +0700

    Week 11 data binding exercise

---
+++ app/src/main/java/com/group/advweek4/view/Interfaces.kt
@@ -0,0 +1,13 @@
+package com.group.advweek4.view
+
+import android.view.View
+
+interface ButtonDetailClickListener{
+    fun onButtonDetailClick(v:View)
+}
+interface ButtonUpdateClickListener{
+    fun onButtonUpdateClick(v:View)
+}
+interface ButtonNotifClickListener{
+    fun onButtonNotifClick(v:View)
+}
